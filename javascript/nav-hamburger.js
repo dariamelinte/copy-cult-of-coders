cbae2b24000962c35d556858a4f0fdaa7ea6e517
dariamelinte <dariamelinte2003@gmail.com>
2019-07-09 16:27:47 +0300

    hamburger menu

--- javascript/nav-hamburger.js
+++ javascript/nav-hamburger.js
@@ -1,3 +1,26 @@
+// Get modal element
+const modal = document.getElementById('simpleModal');
 
+// Get modal button
+const modalBtn = document.getElementById('modalBtn');
 
+// Get close button
+const closeBtn = document.getElementsByClassName('closeBtn')[0];
 
+// Listen for click
+modalBtn.addEventListener('click', openModal);
+closeBtn.addEventListener('click', closeModal);
+
+// Function to open modal
+function openModal () {
+    modal.style.display = 'block';
+    modal.style.animation = 'slide-in .3s';
+}
+
+// Function to close modal
+function closeModal () {
+    modal.style.animation = 'slide-out .3s';
+    setTimeout(() =>{
+        modal.style.display = 'none';
+    }, 300);
+}
